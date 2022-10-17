e2ed0b2e6e271973783ed44d476124cc3f8a9a3f
Murasakibara <joackimcoulibaly5@gmail.com>
2022-10-17 11:47:06 +0000

    inscription et connexion reuissit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,11 +15,7 @@
      <form method="POST" action="{{ route('register') }}">
             @csrf
 			<h1>Inscrivez-vous</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-			</div>
+			
 			<span>Utiliser votre Email</span>
 			<input type="text" placeholder="Votre Nom" name="nom" />
 			<input type="text" placeholder="Votre prenom" name="prenom" />
