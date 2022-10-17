80fd5c653fc5a720a27413b1969645ac376b94f9
Murasakibara <joackimcoulibaly5@gmail.com>
2022-10-17 10:56:15 +0000

    model catgorie fillable

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SousCategorie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Categorie extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'libelle',
+        'description',
+        'created_at',
+        'updated_at',
+        'photo',
+    ];
+
+    public function Souscat() {
+        return $this->hasMany(SousCategorie::class);
+    }
 }
