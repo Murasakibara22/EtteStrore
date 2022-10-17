560e78d3bd47696a1c2e6927c84ca4bdf3f1f602
Murasakibara <joackimcoulibaly5@gmail.com>
2022-10-17 10:46:52 +0000

    mise en place des fonctions de base

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function index(){
+        return view('dashboard');
+    }
+
+
+
+
+
+    
+    public function deconnexion() {
+        auth()->logout();
+    
+        return redirect('/');
+    }
 }
