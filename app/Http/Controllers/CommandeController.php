06a2d1e864e5bab5c29cc55f99b2e004fd921e51
Murasakibara <joackimcoulibaly5@gmail.com>
2022-10-18 21:53:01 +0000

    liste de toutes les commandes

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 class CommandeController extends Controller
 {
 
+    public function listAllCom(){
 
+    }
 
-
+    
     public function addCart(Request $request){
 
     }
